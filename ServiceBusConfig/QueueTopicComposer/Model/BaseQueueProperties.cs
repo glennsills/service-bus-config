0405e96e787c742cbfa22301a8ab1b784e6c19f4
glennsills <glennsills@gmail.com>
2022-08-15 11:08:14 -0400

    Extended BaseQueueProperties

--- ServiceBusConfig/QueueTopicComposer/Model/BaseQueueProperties.cs
+++ ServiceBusConfig/QueueTopicComposer/Model/BaseQueueProperties.cs
@@ -1,25 +1,23 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using Azure.Messaging.ServiceBus.Administration;
 using QueueTopicComposer.Model.Validators;
 
 namespace QueueTopicComposer.Model
 {
     public class BaseQueueProperties
     {
-        [Required]
-        [StringLength(260)]
-        [QueueName]
-        public string Name { get; set; } = string.Empty;
+        public Boolean EnablePartitioning { get; set; } = false;
 
-        [Required]
-        [Range(typeof (TimeSpan), "0:0:5:0", "10675199.02:48:05.4775807")]
-        public TimeSpan AutoDeleteOnIdle { get; set; }
+        public string ForwardDeadLetteredMessagesTo { get; set; }
+
+        public string ForwardTo { get; set; } = string.Empty;
+
+        public EntityStatus Status { get; set; } = EntityStatus.Active;
+
+        public bool EnableBatchedOperations { get; set; } = true;
 
         public int MaxDeliveryCount { get; set; } = 10;
-
-        public TimeSpan DefaultMessageTimeToLive
-        { get; set;
-        } = TimeSpan.MaxValue;
 
         [Range(typeof (TimeSpan), "0:0:0:20", "7:0:0:0")]
         public TimeSpan DuplicateDetectionHistoryTimeWindow
@@ -28,13 +26,33 @@ namespace QueueTopicComposer.Model
 
         public bool DeadLetteringOnMessageExpiration { get; set; } = false;
 
-        [Range(typeof (TimeSpan), "0:0:1", "0:5:0")]
-        public TimeSpan LockDuration { get; set; } = TimeSpan.FromSeconds(60);
+        [Range(typeof (TimeSpan), "0:0:5:0", "10675199.02:48:05.4775807")]
+        public TimeSpan AutoDeleteOnIdle { get; set; } = TimeSpan.MaxValue;
+
+        public TimeSpan DefaultMessageTimeToLive
+        { get; set;
+        } = System.TimeSpan.MaxValue;
 
         public bool RequiresSession { get; set; } = false;
 
-        public bool MessageForwardingEnabled { get; set; } = false;
+        public bool RequiresDuplicateDetection { get; set; } = false;
 
-        public string ForwardDeadLetteredMessagesTo { get; set; }
+        [Range(1, long.MaxValue)]
+        public long MaxSizeInMegabytes { get; set; } = 1024;
+
+        [Range(typeof (TimeSpan), "0:0:1", "0:5:0")]
+        public TimeSpan LockDuration { get; set; } = TimeSpan.FromSeconds(60);
+
+        [Required]
+        [StringLength(260)]
+        [QueueName]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(1, long.MaxValue)]
+        public long? MaxMessageSizeInKilobytes { get; set; }
+
+        public string UserMetadata { get; set; }
+
+        public bool MessageForwardingEnabled { get; set; } = false;
     }
 }
--- ServiceBusConfig/QueueTopicComposer/Model/Queue.cs
+++ ServiceBusConfig/QueueTopicComposer/Model/Queue.cs
@@ -1,11 +1,4 @@
-using QueueTopicComposer.Model;
-
 namespace QueueTopicComposer.Model
 {
-    public class Queue : BaseQueueProperties
-    {
-        public bool DuplicateDetectionEnabled { get; set; } = false;
-        public bool PartitioningEnabled { get; set; } = false;
-        public int MaxSizeInMegabytes { get; set; } = 2048;
-    }
+    public class Queue : BaseQueueProperties { }
 }
